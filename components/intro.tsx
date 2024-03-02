0d4f64b9c3ff14fae0d966bb4107eb160c8250a9
CatAvadani <catalinaavadani@gmail.com>
2024-03-02 14:42:34 +0100

    Add the contact form .

--- components/intro.tsx
+++ components/intro.tsx
@@ -40,22 +40,11 @@ export default function Intro() {
             shadow-xl'
             />
           </motion.div>
-          <motion.span
-            initial={{ opacity: 0, scale: 0 }}
-            animate={{ opacity: 1, scale: 1 }}
-            transition={{
-              type: "spring",
-              stiffness: 125,
-              delay: 0.1,
-              duration: 0.7,
-            }}
-            className='absolute bottom-0 right-0 text-4xl'
-          ></motion.span>
         </div>
       </div>
 
       <motion.h1
-        className='mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-3xl'
+        className='mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-xl'
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{
@@ -95,16 +84,16 @@ export default function Intro() {
         </Link>
 
         <a
-          className='group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer border border-black/10'
+          className='group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10'
           href='/CV.pdf'
           download
         >
           Download CV{" "}
-          <HiDownload className=' opacity-60 group-hover:translate-y-1 ' />
+          <HiDownload className='opacity-60 group-hover:translate-y-1 transition' />
         </a>
 
         <a
-          className='bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer border border-black/10'
+          className='bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer border borderBlack'
           href='https://www.linkedin.com/in/catalinaava09/'
           target='_blank'
         >
@@ -112,7 +101,7 @@ export default function Intro() {
         </a>
 
         <a
-          className='bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer border border-black/10'
+          className='bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer border borderBlack'
           href='https://github.com/CatAvadani'
           target='_blank'
         >
