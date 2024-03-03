21dd0be4ca2b6eba6299c6e6ab8ece22db19d6dd
CatAvadani <catalinaavadani@gmail.com>
2024-03-03 13:57:07 +0100

    Setup the dark and light mode.

--- components/contact.tsx
+++ components/contact.tsx
@@ -23,7 +23,7 @@ export default function Contact() {
       viewport={{ once: true }}
     >
       <SectionHeading>Contact Me</SectionHeading>
-      <p className=' text-gray-700 -mt-5'>
+      <p className=' text-gray-700 -mt-5 dark:text-white/80'>
         Please contact me directly at{" "}
         <a className='underline' href='mailto:catalina_ava@yahoo.com'>
           catalina_ava@yahoo.com
@@ -31,7 +31,7 @@ export default function Contact() {
         or through this form.
       </p>
       <form
-        className=' mt-10 flex flex-col'
+        className=' mt-10 flex flex-col dark:text-black '
         action={async (formData) => {
           const { data, error } = await sendEmail(formData);
           if (error) {
@@ -42,19 +42,19 @@ export default function Contact() {
         }}
       >
         <input
-          className='px-4 h-14 border borderBlack'
+          className='px-4 h-14 border borderBlack dark:bg-white dark:bg-opacity-80 dark:focus:bg-opacity-100 transition-all dark:outline-none'
           name='senderEmail'
           type='email'
           required
-          maxLength={50}
+          maxLength={500}
           placeholder='Your email'
         />
         <textarea
-          className='h-52 my-3 rounded-lg borderBlack p-4'
+          className='h-52 my-3 rounded-lg borderBlack p-4 dark:bg-white dark:bg-opacity-80 dark:focus:bg-opacity-100 transition-all dark:outline-none'
           name='message'
           placeholder='Your message'
           required
-          maxLength={500}
+          maxLength={5000}
         ></textarea>
         <SubmitBtn />
       </form>
