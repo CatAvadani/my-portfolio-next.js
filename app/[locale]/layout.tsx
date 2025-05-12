4389b83dfcf945d58969d53b30f473f3d2e52b78
CatAvadani <catalinaavadani@gmail.com>
2025-05-12 23:04:24 +0200

    Implement internationalization support and restructure project layout

---
+++ app/[locale]/layout.tsx
@@ -0,0 +1,58 @@
+import Footer from "@/components/footer";
+import Header from "@/components/header";
+import ThemeSwitch from "@/components/theme-switch";
+import ActiveSectionContextProvider from "@/context/active-section.context";
+import ThemeContextProvider from "@/context/theme-context";
+import { routing } from "@/i18n/routing";
+import { hasLocale, NextIntlClientProvider } from "next-intl";
+import { getMessages } from "next-intl/server";
+import { DM_Sans } from "next/font/google";
+import { notFound } from "next/navigation";
+import { Toaster } from "react-hot-toast";
+import "./globals.css";
+
+const dm_sans = DM_Sans({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Catalina Avadani | Personal Portfolio",
+  description: "Front-end Developer",
+};
+
+export default async function RootLayout({
+  children,
+  params,
+}: {
+  children: React.ReactNode;
+  params: { locale: string };
+}) {
+  const { locale } = params;
+
+  if (!hasLocale(routing.locales, locale)) {
+    notFound();
+  }
+
+  const messages = await getMessages({ locale });
+
+  return (
+    <html lang={locale} className="!scroll-smooth">
+      <body
+        className={`${dm_sans.className} bg-slate-50 text-gray-950 relative  pt-28 sm:pt-36 dark:bg-gray-900 dark:text-gray-50 dark: text-opacity-90 `}
+      >
+        <div className="bg-[#fbe2e3] absolute top-[ -6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#614b4b]"></div>
+        <div className="bg-[#dbd7fb] absolute top-[ -1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]"></div>
+
+        <ThemeContextProvider>
+          <NextIntlClientProvider locale={locale} messages={messages}>
+            <ActiveSectionContextProvider>
+              <Header />
+              {children}
+              <Footer />
+              <ThemeSwitch />
+              <Toaster position="top-center" />
+            </ActiveSectionContextProvider>
+          </NextIntlClientProvider>
+        </ThemeContextProvider>
+      </body>
+    </html>
+  );
+}
--- components/intro.tsx
+++ components/intro.tsx
@@ -3,6 +3,7 @@
 import { useActiveSectionContext } from "@/context/active-section.context";
 import { useSectionInView } from "@/lib/hooks";
 import { motion } from "framer-motion";
+import { useTranslations } from "next-intl";
 import Image from "next/image";
 import Link from "next/link";
 import { BsArrowRight } from "react-icons/bs";
@@ -12,6 +13,7 @@ import { HiDownload } from "react-icons/hi";
 export default function Intro() {
   const { ref } = useSectionInView("Home", 0.5);
   const { setActiveSection, setTimeOfLastClick } = useActiveSectionContext();
+  const t = useTranslations("Home");
 
   return (
     <section
@@ -38,7 +40,7 @@ export default function Intro() {
               animate={{ opacity: 1, y: 0 }}
               transition={{ duration: 0.6, delay: 0.2 }}
             >
-              Hi, I am Catalina.
+              {t("greeting")}
             </motion.p>
 
             {/* Main heading */}
@@ -48,7 +50,7 @@ export default function Intro() {
               animate={{ opacity: 1, y: 0 }}
               transition={{ duration: 0.6, delay: 0.3 }}
             >
-              Front End Developer
+              {t("title")}
             </motion.h1>
 
             {/* Description */}
@@ -58,10 +60,7 @@ export default function Intro() {
               animate={{ opacity: 1, y: 0 }}
               transition={{ duration: 0.6, delay: 0.4 }}
             >
-              I used to think frontend was about making things look good. Then I
-              learned it’s about making things feel right. Now, I craft
-              interfaces that listen and adapt to users, stories that stick, and
-              code that welcomes everyone in.
+              {t("description")}
             </motion.p>
 
             {/* Buttons section */}
@@ -82,7 +81,7 @@ export default function Intro() {
                     setTimeOfLastClick(Date.now());
                   }}
                 >
-                  Get in Touch{" "}
+                  {t("getInTouch")}
                   <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
                 </Link>
 
@@ -91,7 +90,7 @@ export default function Intro() {
                   href="https://acrobat.adobe.com/id/urn:aaid:sc:EU:6a8510f8-001f-4215-a19c-7ffa50f1fe9e"
                   download
                 >
-                  Download CV{" "}
+                  {t("downloadCV")}
                   <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
                 </a>
               </div>
