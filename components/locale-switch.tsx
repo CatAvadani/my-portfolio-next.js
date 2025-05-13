3f2612db68284336e252a359e33788fd6e84541f
CatAvadani <catalinaavadani@gmail.com>
2025-05-13 02:40:10 +0200

    Fix button text color

--- components/locale-switch.tsx
+++ components/locale-switch.tsx
@@ -18,7 +18,7 @@ export default function LocaleSwitch() {
   return (
     <button
       onClick={toggleLocale}
-      className="fixed bottom-28 right-5 bg-white w-[3rem] h-[3rem] bg-opacity-80 backdrop-blur-[0.5rem] border border-white border-opacity-40 shadow-2xl rounded-full flex items-center justify-center hover:scale-[1.15] active:scale-105 transition-all dark:bg-gray-950 text-lg font-normal text-gray-700 dark:text-gray-200 z-50"
+      className="fixed bottom-28 right-5 bg-white w-[3rem] h-[3rem] bg-opacity-80 backdrop-blur-[0.5rem] border border-white border-opacity-40 shadow-2xl rounded-full flex items-center justify-center hover:scale-[1.15] active:scale-105 transition-all dark:bg-gray-950 text-lg font-normal text-gray-500 dark:text-gray-200 z-50"
       aria-label="Toggle language"
     >
       {currentLocale === "en" ? "Sv" : "En"}
