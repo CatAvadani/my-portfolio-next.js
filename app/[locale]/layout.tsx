0f7d02c2193bac6eb285559a0176c73de447b3f4
CatAvadani <catalinaavadani@gmail.com>
2025-07-16 12:17:21 +0300

    Upgraded @types/react and @types/react-dom to version 19.x - Updated autoprefixer to version 10.4.21 - Upgraded eslint-config-next to version 15.4.1 - Updated next to version 15.4.1 - Upgraded postcss to version 8.5.6 - Updated react and react-dom to version 19.1.x - Upgraded typescript to version 5.8.3

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,21 +1,21 @@
-import Footer from "@/components/footer";
-import Header from "@/components/header";
-import ThemeSwitch from "@/components/theme-switch";
-import ActiveSectionContextProvider from "@/context/active-section.context";
-import ThemeContextProvider from "@/context/theme-context";
-import { routing } from "@/i18n/routing";
-import { hasLocale, NextIntlClientProvider } from "next-intl";
-import { getMessages } from "next-intl/server";
-import { DM_Sans } from "next/font/google";
-import { notFound } from "next/navigation";
-import { Toaster } from "react-hot-toast";
-import "./globals.css";
+import Footer from '@/components/footer';
+import Header from '@/components/header';
+import ThemeSwitch from '@/components/theme-switch';
+import ActiveSectionContextProvider from '@/context/active-section.context';
+import ThemeContextProvider from '@/context/theme-context';
+import { routing } from '@/i18n/routing';
+import { hasLocale, NextIntlClientProvider } from 'next-intl';
+import { getMessages } from 'next-intl/server';
+import { DM_Sans } from 'next/font/google';
+import { notFound } from 'next/navigation';
+import { Toaster } from 'react-hot-toast';
+import './globals.css';
 
-const dm_sans = DM_Sans({ subsets: ["latin"] });
+const dm_sans = DM_Sans({ subsets: ['latin'] });
 
 export const metadata = {
-  title: "Catalina Avadani | Personal Portfolio",
-  description: "Front-end Developer",
+  title: 'Catalina Avadani | Personal Portfolio',
+  description: 'Front-end Developer',
 };
 
 export default async function RootLayout({
@@ -23,9 +23,9 @@ export default async function RootLayout({
   params,
 }: {
   children: React.ReactNode;
-  params: { locale: string };
+  params: Promise<{ locale: string }>;
 }) {
-  const { locale } = params;
+  const { locale } = await params;
 
   if (!hasLocale(routing.locales, locale)) {
     notFound();
@@ -34,12 +34,12 @@ export default async function RootLayout({
   const messages = await getMessages({ locale });
 
   return (
-    <html lang={locale} className="!scroll-smooth">
+    <html lang={locale} className='!scroll-smooth'>
       <body
         className={`${dm_sans.className} bg-slate-50 text-gray-950 relative  pt-28 sm:pt-36 dark:bg-gray-900 dark:text-gray-50 dark: text-opacity-90 `}
       >
-        <div className="bg-[#fbe2e3] absolute top-[ -6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#614b4b]"></div>
-        <div className="bg-[#dbd7fb] absolute top-[ -1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]"></div>
+        <div className='bg-[#fbe2e3] absolute top-[ -6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#614b4b]'></div>
+        <div className='bg-[#dbd7fb] absolute top-[ -1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]'></div>
 
         <ThemeContextProvider>
           <NextIntlClientProvider locale={locale} messages={messages}>
@@ -48,7 +48,7 @@ export default async function RootLayout({
               {children}
               <Footer />
               <ThemeSwitch />
-              <Toaster position="top-center" />
+              <Toaster position='top-center' />
             </ActiveSectionContextProvider>
           </NextIntlClientProvider>
         </ThemeContextProvider>
